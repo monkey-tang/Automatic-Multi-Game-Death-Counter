000f049c3c1ab5a756ee4b8d06a0c5736de7de8e
monkey-tang <monkeytangog@gmail.com>
2026-01-04 08:43:49 -0500

    Add files via upload

---
+++ 1deathcounter/1install_dependencies.bat
@@ -0,0 +1,21 @@
+@echo off
+REM Install Python Dependencies for Death Counter
+REM This batch file installs all required Python packages
+
+echo Installing dependencies for Death Counter...
+echo.
+
+cd /d "%~dp0"
+
+python -m pip install --upgrade pip
+python -m pip install mss pillow pytesseract opencv-python numpy psutil
+
+echo.
+echo Installation complete!
+echo.
+echo Note: You also need to install Tesseract OCR separately:
+echo Download from: https://github.com/UB-Mannheim/tesseract/wiki
+echo Default path: C:\Program Files\Tesseract-OCR\tesseract.exe
+echo.
+pause
+
